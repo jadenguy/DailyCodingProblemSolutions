33eea2f53455bd797aaa1c65cc6b0b31e55ffa66
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-12 15:35:09 -0400

    Test built, working on logic

---
+++ src/Common/Solution9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class Solution9
+    {
+        private class Candidate
+        {
+
+            protected Candidate(int value, int[] path)
+            {
+                this.Value = value;
+                this.Path = path;
+
+            }
+            public int Value { get; private set; }
+            public int[] Path { get; private set; }
+
+            public override string ToString()
+            {
+                return $"{Value} at {Path}";
+            }
+        }
+        public static int LargestSumNonAdjacent(int[] list)
+        {
+            var ret = 0;
+            var candidates = new Stack<Candidate>();
+            foreach (var item in list)
+            {
+                
+            }
+            return ret;
+        }
+    }
+}
---
+++ tests/Common.Test/Test9.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Test9
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(new int[] { 2, 4, 6, 2, 5 }, 13)]
+        [TestCase(new int[] { 5, 1, 1, 5 }, 10)]
+        public void Problem9(int[] list, int answer)
+        {
+            //-- Arrange
+            var expected = answer;
+
+            //-- Act
+            var actual = Common.Solution9.LargestSumNonAdjacent(list);
+
+            //-- Assert
+            Assert.AreEqual(actual,expected);
+
+        }
+    }
+}
