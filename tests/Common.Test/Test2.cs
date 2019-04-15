2d3bf829cd2b7e141517003dd1689f59628e2c40
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-15 10:47:29 -0400

    Added bad tests cases

--- tests/Common.Test/Test2.cs
+++ tests/Common.Test/Test2.cs
@@ -13,6 +13,8 @@ namespace Tests
         [Test]
         [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 120, 60, 40, 30, 24 })]
         [TestCase(new int[] { 3, 2, 1 }, new int[] { 2, 3, 6 })]
+        [TestCase(new int[] { 1 }, new int[] { 0 })]
+        [TestCase(new int[] {  }, new int[] {  })]
         public void Problem2(int[] list, int[] answer)
         {
             //-- Arrange
@@ -23,7 +25,6 @@ namespace Tests
 
             //-- Assert
             Assert.AreEqual(actual, expected);
-
         }
     }
 }
