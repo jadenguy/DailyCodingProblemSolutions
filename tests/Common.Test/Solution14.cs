f6c6778c5cb754bf7e692423bd76573b043ae266
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-18 09:05:09 -0400

    Solution14 Done

---
+++ tests/Common.Test/Solution14.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Common
+{
+    public class Solution14
+    {
+        public static double CalculatePi(int steps)
+        {
+            var rand = new Random();
+            var xPoints = new double[steps];
+            var yPoints = new double[steps];
+            var inCircleCount = 0;
+            for (int i = 0; i < steps; i++)
+            {
+                xPoints[i] = rand.NextDouble() ;
+                yPoints[i] = rand.NextDouble() ;
+            }
+            for (int i = 0; i < steps; i++)
+            {
+                var xSquared = Math.Pow(xPoints[i],2);
+                var ySquared = Math.Pow(yPoints[i],2);
+                var sqrt = Math.Sqrt(xSquared + ySquared);
+                if (sqrt < 1 ) { inCircleCount++; }
+
+            }
+            var ret = 4d * inCircleCount / steps;
+            return ret;
+        }
+    }
+}
---
+++ tests/Common.Test/Test14.cs
@@ -0,0 +1,27 @@
+// The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+// Hint: The basic equation of a circle is x2 + y2 = r2.
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    [TestFixture]
+    public class Test14
+    {
+
+        [Test]
+        [TestCase(10000000)]
+        public void Problem4(int steps)
+        {
+            //-- Arrange
+            var expected = System.Math.PI;
+
+            //-- Act
+            var actual = Solution14.CalculatePi(steps);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual,0.001);
+        }
+    }
+}
