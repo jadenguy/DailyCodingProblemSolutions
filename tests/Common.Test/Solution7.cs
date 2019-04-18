118ec52bd843d5c8ec09fc2f955cfb3ab4f0a8c5
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-18 16:34:29 -0400

    Solution7

---
+++ tests/Common.Test/Solution7.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+
+namespace Common
+{
+    public static class Solution7
+    {
+        public static int DecodeCount(string code)
+        {
+            var ret = 1;
+            var reverseArray = code.Reverse().Select(x => Convert.ToInt32(x) - 48).ToArray();
+            ret += Process(reverseArray, 0);
+            return ret;
+        }
+        private static int Process(int[] reverseArray, int index)
+        {
+            var ret = 0;
+            int length = reverseArray.Length;
+            if (index < length)
+            {
+                if (reverseArray[index] == 0) { ret--; }
+                if (index < length - 1)
+                {
+                    var nextNumber = reverseArray[index + 1];
+                    if (nextNumber == 1 || (nextNumber == 2 && reverseArray[index] <= 2))
+                    {
+                        ret++;
+                        ret += Process(reverseArray, index + 2);
+                    }
+                }
+                ret += Process(reverseArray, index + 1);
+            }
+            return ret;
+        }
+    }
+}
