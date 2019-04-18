37f4688b3a4a8a75f88274ce4942de1e97e3e358
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-18 16:34:17 -0400

    Not really c#

---
+++ tests/Common.Test/Test6.cs
@@ -0,0 +1,29 @@
+// An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+// If using a language that has no pointers (such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
+
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    [TestFixture]
+    public class Test6
+    {
+
+        [Test]
+        public void Problem6()
+        {
+            //-- Arrange
+
+
+            //-- Act
+
+
+            //-- Assert
+            System.Diagnostics.Debug.WriteLine("Not bothering");
+            System.Console.WriteLine("Not bothering");
+            Assert.Pass();
+        }
+    }
+}
