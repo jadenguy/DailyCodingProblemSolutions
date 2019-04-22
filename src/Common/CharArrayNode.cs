43e646837f17b0bdc6bacbcd9b08d05cdbc26b25
Alfredo Inostroza <jadenguy@gmail.com>
2019-04-22 14:32:07 -0400

    Added longer test cases Comparison of solutions

---
+++ src/Common/CharArrayNode.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class CharArrayNode : IDictionary<char, CharArrayNode>
+    {
+        public string Word { get; set; }
+        public bool WordEndsHere { get; set; }
+        public CharArrayNode(char? letter, string wordUntilNow)
+        {
+            this.Letter = letter;
+            this.Word = wordUntilNow + letter.ToString();
+        }
+        private Dictionary<char, CharArrayNode> nextLetter { get; set; } = new Dictionary<char, CharArrayNode>();
+        public char? Letter { get; set; }
+        public ICollection<char> Keys => ((IDictionary<char, CharArrayNode>)nextLetter).Keys;
+        public ICollection<CharArrayNode> Values => ((IDictionary<char, CharArrayNode>)nextLetter).Values;
+        public int Count => ((IDictionary<char, CharArrayNode>)nextLetter).Count;
+        public bool IsReadOnly => ((IDictionary<char, CharArrayNode>)nextLetter).IsReadOnly;
+        public CharArrayNode this[char key] { get => ((IDictionary<char, CharArrayNode>)nextLetter)[key]; set => ((IDictionary<char, CharArrayNode>)nextLetter)[key] = value; }
+        public void Add(char key, CharArrayNode value)
+        {
+            ((IDictionary<char, CharArrayNode>)nextLetter).Add(key, value);
+        }
+
+        public bool ContainsKey(char key)
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).ContainsKey(key);
+        }
+        public bool Remove(char key)
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).Remove(key);
+        }
+        public bool TryGetValue(char key, out CharArrayNode value)
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).TryGetValue(key, out value);
+        }
+        public void Add(KeyValuePair<char, CharArrayNode> item)
+        {
+            ((IDictionary<char, CharArrayNode>)nextLetter).Add(item);
+        }
+        public void Clear()
+        {
+            ((IDictionary<char, CharArrayNode>)nextLetter).Clear();
+        }
+        public bool Contains(KeyValuePair<char, CharArrayNode> item)
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).Contains(item);
+        }
+        public void CopyTo(KeyValuePair<char, CharArrayNode>[] array, int arrayIndex)
+        {
+            ((IDictionary<char, CharArrayNode>)nextLetter).CopyTo(array, arrayIndex);
+        }
+        public bool Remove(KeyValuePair<char, CharArrayNode> item)
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).Remove(item);
+        }
+        public IEnumerator<KeyValuePair<char, CharArrayNode>> GetEnumerator()
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IDictionary<char, CharArrayNode>)nextLetter).GetEnumerator();
+        }
+        public override string ToString()
+        {
+            var ret = string.Empty;
+            if (Letter != null) { ret = $"{Word} {WordEndsHere} {nextLetter.Count}"; }
+            else { ret = $"this is the root {nextLetter.Count}"; }
+            return ret;
+        }
+        public void AddNextLetter(char character)
+        {
+            if (!ContainsKey(character))
+            { Add(character, new CharArrayNode(character, Word)); }
+        }
+    }
+}
