2cc04b23acdfb2bee88c9e6316e29ca3112db61a
Alfredo Inostroza <jadenguy@gmail.com>
2019-05-20 13:41:44 -0400

    46 done

---
+++ tests/Common.Test/Test46.cs
@@ -0,0 +1,24 @@
+﻿// Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum length, return any one.
+// For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
+
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test46
+    {
+        [SetUp]
+        public void Setup() { }
+        [Test]
+        [TestCase("bananas", "anana")]
+        public void Problem25(string text, string anagram)
+        {
+            //-- Arrange
+            var expected = anagram;
+            //-- Act
+            var actual = Solution46.FindLongestAnagram(text);
+            //-- Assert
+            Assert.AreEqual(expected.Length, actual.Length);
+        }
+    }
+}
