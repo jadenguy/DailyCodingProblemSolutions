82e7ffa7d5b3be29e5db93e0c81ece68394e4317
Alfredo Inostroza <jadenguy@gmail.com>
2019-06-05 11:38:24 -0400

    most of the way done, off by 1 bug

---
+++ src/Common/Solution26.cs
@@ -0,0 +1,25 @@
+using System;
+using Common.Node;
+
+namespace Common
+{
+    public static class Solution26
+    {
+        public static void RemoveNthLastElement(LinkedListNode list, int wanted)
+        {
+            var x = list;
+            var y = list;
+            for (int i = 0; i <= wanted; i++)
+            {
+                x = x.Next;
+            }
+            System.Diagnostics.Debug.WriteLine(x?.Print());
+            while (x != null && x.Next != null)
+            {
+                x = x.Next;
+                y = y.Next;
+            }
+            y.Next = y.Next.Next;
+        }
+    }
+}
