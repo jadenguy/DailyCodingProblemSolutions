02d3988806f5d2f6d3912f366bd13604d4ae546c
Alfredo Inostroza <jadenguy@gmail.com>
2019-06-07 09:54:55 -0400

    Knight's Tour

---
+++ tests/Common.Test/Test64.cs
@@ -0,0 +1,25 @@
+﻿// A knight's tour is a sequence of moves by a knight on a chessboard such that all squares are visited once.
+// Given N, write a function to return the number of knight's tours on an N by N chessboard.
+
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test64
+    {
+        [SetUp]
+        public void Setup() { }
+        [Test]
+        public void Problem64()
+        {
+            //-- Arrange
+            var expected = 26534728821064;
+
+            //-- Act
+            var actual = Solution64.KnightTours().Count();
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
