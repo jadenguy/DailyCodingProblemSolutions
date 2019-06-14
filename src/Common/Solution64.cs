4d0872f52c72017db3cfe51d338fc90c33978c32
Alfredo Inostroza <jadenguy@gmail.com>
2019-06-14 10:24:04 -0400

    More tests, passing slow solution

--- src/Common/Solution64.cs
+++ src/Common/Solution64.cs
@@ -1,40 +1,101 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Common
 {
     public static class Solution64
     {
-        public static int[][,] KnightTours(int boardSize = 8)
+        public static IEnumerable<int[,]> KnightToursEveryCell(int boardSize = 5)
         {
             var ret = new List<int[,]>();
             var board = BlankBoard(boardSize);
-            // for (int x = 0; x < boardSize; x++)
-            // {
-            // for (int y = 0; y < boardSize; y++)
-            // {
-            var startPosition = (2, 2);
-            var nextMove = FindNextKnightMove(board, startPosition);
-            System.Diagnostics.Debug.WriteLine(new { startPosition, moves = nextMove.Count() });
-            // }
-            // }
-            return ret.ToArray();
+            for (int x = 0; x < boardSize; x++)
+            {
+                for (int y = 0; y < boardSize; y++)
+                {
+                    var startPosition = (x, y);
+                    var results = KnightTourFrom((int[,])board.Clone(), startPosition, 1).ToArray();
+                    ret.AddRange(results);
+                }
+            }
+            return ret;
         }
-        private static IEnumerable<(int, int)> FindNextKnightMove(int[,] board, (int x, int y) p)
+        public static string PrintBoard(this int[,] board)
+        {
+            var sb = new StringBuilder();
+            for (int x = 0; x <= board.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= board.GetUpperBound(1); y++)
+                {
+                    sb.Append($"{board[x, y],3}");
+                }
+                sb.AppendLine();
+            }
+            System.Diagnostics.Debug.WriteLine(sb.ToString());
+            System.Console.WriteLine();
+            System.Console.WriteLine(sb.ToString());
+            return sb.ToString();
+        }
+        public static IEnumerable<int[,]> KnightTourFrom(int[,] board, (int x, int y) startPosition, int placeInList)
+        {
+            board[startPosition.x, startPosition.y] = placeInList;
+            if (placeInList == board.Length)
+            {
+                // System.Diagnostics.Debug.WriteLine(board.PrintBoard());
+                // System.Diagnostics.Debug.WriteLine($"{placeInList} at {startPosition}");
+                yield return board;
+            }
+            else
+            {
+                var nextMove = FindNextKnightMove(board, startPosition);
+                foreach (var move in nextMove)
+                {
+                    foreach (var item in KnightTourFrom((int[,])board.Clone(), move, placeInList + 1))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public static int[,] SelectElements(int size)
+        {
+            int[,] board;
+            bool odd = true;
+            if (size % 2 == 0) { odd = false; }
+            board = new int[size, size];
+            for (int xPos = 0; xPos < size / 2; xPos++)
+            {
+                for (int yPos = xPos; yPos < size / 2; yPos++)
+                {
+                    var value = 8;
+                    board[xPos, yPos] = -1;
+                    if (xPos == yPos)
+                    {
+                        value = 4;
+                        if (odd && xPos == 1 + (size / 2)) { value = 1; }
+                    }
+                    board[xPos, yPos] = value;
+                }
+            }
+            return board;
+        }
+        private static IEnumerable<(int, int)> FindNextKnightMove(int[,] board, (int x, int y) position)
         {
             var xMax = board.GetUpperBound(0);
             var yMax = board.GetUpperBound(1);
             var pointList = new List<(int, int)>();
 
-            int x = p.x;
+            int x = position.x;
             var xp1 = x < xMax;
             var xp2 = x < xMax - 1;
             var xn1 = x > 0;
             var xn2 = x > 1;
 
-            int y = p.y;
+            int y = position.y;
             var yp1 = y < yMax;
             var yp2 = y < yMax - 1;
             var yn1 = y > 0;
@@ -50,13 +111,13 @@ namespace Common
             if (xn2 && yp1) pointList.Add((x - 2, y + 1));
             if (xn2 && yn1) pointList.Add((x - 2, y - 1));
 
-            foreach ((int x, int y) item in pointList)
+            foreach ((int x, int y) point in pointList)
             {
-                if (board[item.x, item.y] == 0) { yield return (item.x, item.y); }
+                if (board[point.x, point.y] == 0) { yield return point; }
             }
 
         }
-        private static int[,] BlankBoard(int boardSize = 8)
+        public static int[,] BlankBoard(int boardSize = 8)
         {
             var board = new int[boardSize, boardSize];
             return board;
--- tests/Common.Test/Test64.cs
+++ tests/Common.Test/Test64.cs
@@ -1,6 +1,7 @@
 ﻿// A knight's tour is a sequence of moves by a knight on a chessboard such that all squares are visited once.
 // Given N, write a function to return the number of knight's tours on an N by N chessboard.
 
+using System.Linq;
 using NUnit.Framework;
 
 namespace Common.Test
@@ -10,16 +11,54 @@ namespace Common.Test
         [SetUp]
         public void Setup() { }
         [Test]
+        [TestCase(5, 2, 2, 64)]
+        [TestCase(5, 1, 1, 56)]
+        [TestCase(1, 0, 0, 1)]
+        public void OnePositionKnightTour(int size, int startX, int startY, int answers)
+        {
+            //-- Arrange
+            var expected = answers;
+
+            //-- Act
+            int[][,] boards = Solution64.KnightTourFrom(Solution64.BlankBoard(size), (startX, startY), 1).ToArray();
+
+            var actual = boards.Length;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
         public void Problem64()
         {
             //-- Arrange
-            // var expected = 26534728821064;
+            var expected = 1;
 
             //-- Act
-            var actual = Solution64.KnightTours(5).Length;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var actual = Solution64.KnightToursEveryCell(1).Count();
+            System.Diagnostics.Debug.WriteLine(watch.ElapsedMilliseconds);
+            System.Console.WriteLine(watch.ElapsedMilliseconds);
 
             //-- Assert
-            // Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        [TestCase(4, 28)]
+        [TestCase(5, 25)]
+        [TestCase(6, 36)]
+        [TestCase(7, 49)]
+        public void PickArrayElements(int size, int square) 
+        //  I could save between 2 and 8 rounds of calculation by returning the set of result families that could be rotated and flipped.
+        {
+            var selected = Solution64.SelectElements(size);
+            var selectionSum = 0;
+            foreach (var item in selected)
+            {
+                selectionSum += item;
+            }
+            selected.PrintBoard();
+            Assert.AreEqual(size * size, square, $"Your TestCase is wrong, k = {square}, should be {size * size}.");
+            Assert.AreEqual(square, selectionSum);
         }
     }
 }
