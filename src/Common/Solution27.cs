bb48b85dbbc973d6a1da338532b74d2ab2d96886
Alfredo Inostroza <jadenguy@gmail.com>
2019-06-14 14:29:16 -0400

    Test made for 27

---
+++ src/Common/Solution27.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Common
+{
+    public static class Solution27
+    {
+        public static bool Validate(this string input)
+        {
+            var ret = false;
+            return ret;
+        }
+    }
+}
