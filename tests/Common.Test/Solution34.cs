76d06d0eb355215b71a96342395155bea02ee67e
Alfredo Inostroza <jadenguy@gmail.com>
2019-07-12 14:18:52 -0400

    34 Right idea, wrong direction I was finding palindrom subestings i should just delete any matched characters

--- tests/Common.Test/Solution34.cs
+++ tests/Common.Test/Solution34.cs
@@ -1,15 +1,64 @@
 using System;
+using Common.Extensions;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Common.Test
+namespace Common
 {
     public class Solution34
     {
-
         enum Direction
         {
             Left = 0, Right = 1
         }
         public static string Palindromize(string input)
+        {
+            // return PalindromizeOld(input);
+            var substrings = AllSubStrings(input);
+            var masks = substrings.Select(t => new { From = t.from, Text = t.text, Mask = PalindromeMask(t.text) }).ToArray();
+            var mostMaskCharacters = masks.OrderBy(k => k.Mask.Length).OrderByDescending(k => k.Mask.Where(b => b).Count());
+            foreach (var substring in mostMaskCharacters)
+            {
+                System.Diagnostics.Debug.WriteLine("");
+                System.Diagnostics.Debug.WriteLine(substring.From);
+                System.Diagnostics.Debug.WriteLine(string.Join('\t', substring.Text.ToCharArray()));
+                System.Diagnostics.Debug.WriteLine(string.Join('\t', substring.Mask));
+            }
+            return input;
+        }
+        private static bool[] PalindromeMask(string input)
+        {
+            var ret = new bool[input.Length];
+            var i = 0;
+            while (i <= input.Length / 2)
+            {
+                int leftIndex = i;
+                int rightIndex = input.Length - 1 - i;
+                char leftChar = input[leftIndex];
+                char rightChar = input[rightIndex];
+                if (leftChar == rightChar)
+                {
+                    ret[leftIndex] = true;
+                    ret[rightIndex] = true;
+                }
+                i++;
+            }
+
+            return ret;
+        }
+
+        private static IEnumerable<(string text, int from)> AllSubStrings(string input)
+        {
+            for (int a = 0; a < input.Length; a++)
+            {
+                for (int b = 1; b <= input.Length - a; b++)
+                {
+                    yield return (input.Substring(a, b), a);
+                }
+            }
+        }
+
+        public static string PalindromizeOld(string input)
         {
             var i = 0;
             while (i <= input.Length / 2)
@@ -22,18 +71,17 @@ namespace Common.Test
                 char insertChar = leftChar;
                 int insertIndex = rightIndex;
                 Direction insertDirection = Direction.Right;
-                switch (comparison)
+                if (comparison != 0)
                 {
-                    case 1:
+                    if (comparison == 1)
+                    {
                         insertChar = rightChar;
                         insertIndex = leftIndex;
                         insertDirection = Direction.Left;
-                        break;
-                    case 0:
-                        i++;
-                    break;
+                    }
+                    input = addLetterIn(input, insertChar, insertIndex, insertDirection);
                 }
-                if (comparison != 0) { input = addLetterIn(input, insertChar, insertIndex, insertDirection); }
+                i++;
             }
             return input;
         }
@@ -52,7 +100,6 @@ namespace Common.Test
             }
             return direction;
         }
-
         private static string addLetterIn(string input, char letter, int place, Direction direction)
         {
             input = input.Insert(place + (int)direction, letter.ToString());
