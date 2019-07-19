2ba5dc4566a96f8aa412bd182290dc28c1efd520
Alfredo Inostroza <jadenguy@gmail.com>
2019-07-19 12:07:18 -0400

    40 Some clever bloke online showed me how I'm deconstructing it I'm creating a set theory class to understand what happened I'll see if I can replicate it

--- tests/Common.Test/Test040.cs
+++ tests/Common.Test/Test040.cs
@@ -1,3 +1,7 @@
+// Given an array of integers where every integer occurs three times except for one integer, which only occurs once, find and return the non-duplicated integer.
+// For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
+// Do this in O(N) time and O(1) space.
+
 using NUnit.Framework;
 
 namespace Common.Test
@@ -9,13 +13,14 @@ namespace Common.Test
         // [TearDown]
         // public void TearDown() { }
         [Test]
-        public void Problem040(int n, int results)
+        [TestCase(new int[] { 6, 1, 3, 3, 3, 6, 6 }, 1)]
+        public void Problem040(int[] array, int results)
         {
             //-- Arrange
             var expected = results;
 
             //-- Act
-            var actual = Solution040.DoThis();
+            var actual = Solution040.FindUnique(array);
 
             //-- Assert
             Assert.AreEqual(expected, actual);
