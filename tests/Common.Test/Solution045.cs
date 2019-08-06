d1ad72fe688a87f3a8fb38936b443930f8c8cac5
Alfredo Inostroza <jadenguy@gmail.com>
2019-08-06 11:55:37 -0400

    Proper test 45 written, solved

--- tests/Common.Test/Solution045.cs
+++ tests/Common.Test/Solution045.cs
@@ -1,14 +1,26 @@
 using System;
+using System.Linq;
 
 namespace Common
 {
     public class Solution045
     {
-        public static int Rand5() => (new Random()).Next(1, 5);
-
-        internal static object Rand7()
+        public static int Rand5(int seed = 0)
         {
-            throw new NotImplementedException();
+            Random rand;
+            if (seed == 0)
+            { rand = new Random(); }
+            else { rand = new Random(seed); }
+            return rand.Next(1, 6);
+        }
+        public static int Rand7(int seed = 0)
+        {
+            Random rand;
+            if (seed == 0)
+            { rand = new Random(); }
+            else { rand = new Random(seed); }
+            var x = 1 + (Enumerable.Range(0, 7).Select(k => Rand5(rand.Next())).Sum() % 7);
+            return x;
         }
     }
 }
--- tests/Common.Test/Test045.cs
+++ tests/Common.Test/Test045.cs
@@ -1,5 +1,6 @@
 ﻿// Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a function rand7() that returns an integer from 1 to 7 (inclusive).
 
+using System;
 using System.Linq;
 using Common.Extensions;
 using NUnit.Framework;
@@ -8,32 +9,40 @@ namespace Common.Test
 {
     public class Test045
     {
+        Random rand = new Random();
+        private const double stDevTolerance = .1;
+        private const double avgTolerance = .1;
+        private const int nRuns = 1000;
+
         [SetUp]
-        public void Setup() { }
+        // public void Setup() { }
         [Test]
-        public void Problem045(string text, string anagram)
+        public void Problem045()
         {
             //-- Arrange
-            var expected = Enumerable.Range(0, 1000).SelectMany(k => Enumerable.Range(1, 7)).Select(z => (double)z).ToArray();
+            var expected = Enumerable.Range(0, nRuns).SelectMany(k => Enumerable.Range(1, 7)).Select(z => (double)z).ToArray();
+
             //-- Act
-            var actual = Enumerable.Range(0, 1000).SelectMany(k => Enumerable.Range(1, 7).Select(v => Solution045.Rand7())).Select(z => (double)z).ToArray();
+            var actual = Enumerable.Range(0, nRuns).SelectMany(k => Enumerable.Range(1, 7).Select(v => Solution045.Rand7(rand.Next()))).Select(z => (double)z).ToArray();
 
             //-- Assert
-            Assert.AreEqual(expected.SampleStandardDeviation(), actual.SampleStandardDeviation(), .01);
-            Assert.AreEqual(expected.Average(), actual.Average(), .01);
+
+            Assert.AreEqual(expected.SampleStandardDeviation(), actual.SampleStandardDeviation(), stDevTolerance, "standard deviation wrong");
+            Assert.AreEqual(expected.Average(), actual.Average(), avgTolerance, "average wrong");
 
         }
         [Test]
-        public void Problem045Control(string text, string anagram)
+        public void Problem045Control()
         {
             //-- Arrange
-            var expected = Enumerable.Range(0, 1000).SelectMany(k => Enumerable.Range(1, 5)).Select(z => (double)z).ToArray();
+            var expected = Enumerable.Range(0, nRuns).SelectMany(k => Enumerable.Range(1, 5)).Select(z => (double)z).ToArray();
+
             //-- Act
-            var actual = Enumerable.Range(0, 1000).SelectMany(k => Enumerable.Range(1, 5).Select(v => Solution045.Rand5())).Select(z => (double)z).ToArray();
+            var actual = Enumerable.Range(0, nRuns).SelectMany(k => Enumerable.Range(1, 5).Select(v => Solution045.Rand5(rand.Next()))).Select(z => (double)z).ToArray();
 
             //-- Assert
-            Assert.AreEqual(expected.SampleStandardDeviation(), actual.SampleStandardDeviation(), .01);
-            Assert.AreEqual(expected.Average(), actual.Average(), .01);
+            Assert.AreEqual(expected.SampleStandardDeviation(), actual.SampleStandardDeviation(), stDevTolerance, "standard deviation wrong");
+            Assert.AreEqual(expected.Average(), actual.Average(), avgTolerance, "average wrong");
         }
     }
 }
