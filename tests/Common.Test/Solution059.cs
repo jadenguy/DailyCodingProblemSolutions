d0c3422048c868f1b4cf561cca2de184fdab16c0
Alfredo Inostroza <jadenguy@gmail.com>
2019-08-21 10:58:37 -0400

    59 Solution

--- tests/Common.Test/Solution059.cs
+++ tests/Common.Test/Solution059.cs
@@ -1,30 +1,80 @@
-using System.Collections.Generic;
+using System;
 using System.Linq;
 
 namespace Common.Test
 {
     public class Solution059
     {
-        public static int TransferFile(byte[] localFile, byte[] remoteFile, int blockSize = 1000)
+        public static int TransferFile(byte[] localFile, byte[] remoteFile, object fileSystem, object connection, int blockSize = 1000)
         {
             var ret = 0;
-            var localFileTemp = new List<byte>();
-            int length = remoteFile.Length;
-            for (int i = 0; i < length / blockSize; i++)
+            int tx = 0;
+            int rx = 0;
+            // var localFileTemp = new List<byte>();
+            // one byte for the request message
+            tx += 1;
+            int length = RequestRemoteFileSize(connection, remoteFile);
+            // four bytes for the received int size
+            rx += 4;
+            localFile = resizeLocalFile(localFile, length);
+            int blockCount = length / blockSize;
+            for (int i = 0; i < blockCount; i++)
             {
-                var localBlock = GetBlock(localFile, blockSize, blockSize * i).ToArray();
-                byte localCheckSum = checkSum(localBlock);
-                System.Diagnostics.Debug.WriteLine(localCheckSum);
+                var localBlock = GetBlock(fileSystem, localFile, blockSize, i);
+                // one byte for the request message
+                tx++;
+                byte remoteCheckSum = GetRemoteCheckSum(connection, remoteFile, blockSize, i);
+                // one byte back
+                rx++;
+                while (checkSum(localBlock) != remoteCheckSum)
+                {
+                    tx++;
+                    var remoteBlock = GetRemoteBlock(connection, remoteFile, blockSize, i);
+                    rx += blockSize;
+                    ReplaceBlock(fileSystem, localFile, blockSize, i, remoteBlock);
+                    localBlock = GetBlock(fileSystem, localFile, blockSize, i);
+                }
             }
+            ret = tx + rx;
             return ret;
         }
 
-        private static IEnumerable<byte> GetBlock(byte[] localFile, int blockSize, int blockStart)
+        private static byte[] resizeLocalFile(byte[] localFile, int length)
         {
-            return localFile.Skip(blockStart).Take(blockSize);
+            int localLength = localFile.Length;
+            if (length != localLength)
+            {
+                var temp = new byte[length];
+                Array.Copy(localFile, temp, Math.Min(length, localLength));
+                localFile = temp;
+            }
+
+            return localFile;
         }
 
-        private static byte checkSum(IEnumerable<byte> block)
+        private static void ReplaceBlock(object fileSystem, byte[] localFile, int blockSize, int i, byte[] remoteBlock)
+        {
+            Array.Copy(remoteBlock, 0, localFile, blockSize * i, blockSize);
+        }
+        private static byte[] GetRemoteBlock(object connection, byte[] file, int blockSize, int i)
+        {
+            return GetBlock(connection, file, blockSize, i);
+        }
+        private static byte GetRemoteCheckSum(object connection, byte[] remoteFile, int blockSize, int i)
+        {
+            var block = GetBlock(connection, remoteFile, blockSize, i).ToArray();
+            byte remoteCheckSum = checkSum(block);
+            return remoteCheckSum;
+        }
+        private static int RequestRemoteFileSize(object connection, byte[] remoteFile)
+        {
+            return remoteFile.Length;
+        }
+        private static byte[] GetBlock(object fileSystem, byte[] file, int blockSize, int index)
+        {
+            return file.Skip(blockSize * index).Take(blockSize).ToArray();
+        }
+        private static byte checkSum(byte[] block)
         {
             byte checksum = default(byte);
             foreach (var byteItem in block) // using xor as rudimentary checksum
--- tests/Common.Test/Test059.cs
+++ tests/Common.Test/Test059.cs
@@ -19,25 +19,47 @@ namespace Common.Test
         public void Problem059()
         {
             //-- Arrange
-            int totalBytes = 10000;
-            int uniqueBytes = 4321;
-            int repeatBytes = totalBytes - uniqueBytes;
+            int totalBytes = 1000000;
+            int deltaBytes = 4321;
             const int blockSize = 1000;
 
             var rand = new Random();
             var seed = rand.Next();
-            var file1 = RandomFileMD5(seed, totalBytes);
-            var file2 = file1.Take(repeatBytes).Union(RandomFileMD5(rand.Next(), uniqueBytes)).ToArray();
-            var expected = uniqueBytes;
+            var file1 = RandomFile(seed, totalBytes);
+            byte[] file2 = RandomDifferentFile(file1, totalBytes, deltaBytes, rand.Next());
+            var expected = deltaBytes + (totalBytes / blockSize);
 
             //-- Act
-            var actual = Solution059.TransferFile(file1, file2, blockSize);
+
+            object fileSystem = new object();
+            object connection = new object();
+            var actual = Solution059.TransferFile(file1, file2, fileSystem, connection, blockSize); //minimum overhead equal to the block count plus file size
 
             // //-- Assert
             Assert.AreEqual(file1, file2);
-            Assert.AreEqual(expected, actual, blockSize);
+            bool savedFileTransferBytes = totalBytes.CompareTo(actual) > 0;
+            Assert.IsTrue(savedFileTransferBytes, "transfer smaller than whole file");
+            if (savedFileTransferBytes)
+            {
+                System.Console.WriteLine(expected - actual);
+                Assert.AreEqual(expected, actual, blockSize * 2, "within 2 blocks of unique bytes");
+            }
         }
-        private static byte[] RandomFileMD5(int seed, int elementCount)
+
+        private static byte[] RandomDifferentFile(byte[] file, int totalBytes, int uniqueBytes, int seed)
+        {
+            var ret = new byte[totalBytes];
+            int repeatBytes = totalBytes - uniqueBytes;
+            var repeatedStart = file.Take(repeatBytes / 2).ToArray();
+            var repeatedEnd = file.Skip(uniqueBytes + (repeatBytes / 2)).Take(repeatBytes - repeatBytes / 2).ToArray();
+            var uniquePart = RandomFile(seed, uniqueBytes);
+            Array.Copy(repeatedStart, ret, repeatBytes / 2);
+            Array.Copy(uniquePart, 0, ret, repeatBytes / 2, uniqueBytes);
+            Array.Copy(repeatedEnd, 0, ret, uniqueBytes + (repeatBytes / 2), repeatBytes - repeatBytes / 2);
+            return ret;
+        }
+
+        private static byte[] RandomFile(int seed, int elementCount)
         {
             var rand = new Random(seed);
             byte[] file = new byte[elementCount];
