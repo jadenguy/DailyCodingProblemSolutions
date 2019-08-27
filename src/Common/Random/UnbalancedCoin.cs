91b4f2eff1bb7d95e9e0c7d05b8d7f46a9bc4b0a
Alfredo Inostroza <jadenguy@gmail.com>
2019-08-27 09:40:55 -0400

    Test 66

---
+++ src/Common/Random/UnbalancedCoin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Common
+{
+
+    public class UnbalancedCoin
+    {
+        Random rand;
+        int probAnchor;
+        public UnbalancedCoin(int seed = 0)
+        {
+            if (seed == 0) { rand = new Random(); }
+            else { rand = new Random(seed); }
+            probAnchor = rand.Next();
+        }
+        public bool TossBiased() => rand.Next() < probAnchor;
+        public bool TossUnbiased()
+        {
+            bool a, b;
+            int i = 0;
+            do
+            {
+                a = TossBiased();
+                b = TossBiased();
+                i++;
+            } while (a == b);
+            System.Diagnostics.Debug.WriteLine(i);
+            return a.CompareTo(b) > 0;
+        }
+    }
+}
---
+++ tests/Common.Test/Test066.cs
@@ -0,0 +1,40 @@
+﻿// Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the bias of the coin.
+// Write a function to simulate an unbiased coin toss.
+
+
+using System.Linq;
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test066
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown(){}
+        [Test]
+        public void Problem066()
+        {
+            //-- Arrange
+            var length = 1000;
+            var expectedAverage = .5;
+            var expectedStdDev = .5;
+            var coin = new UnbalancedCoin();
+            bool[] actual = new bool[length];
+
+            //-- Act
+            for (int i = 0; i < length; i++)
+            {
+                actual[i] = coin.TossUnbiased();
+            }
+            var actualNumeric = actual.Select(k => k ? 0d : 1d);
+            var actualAverage = actualNumeric.Average();
+            var actualStdDev = actualNumeric.PopulationStandardDeviation();
+
+
+            //-- Assert
+            Assert.AreEqual(expectedAverage, actualAverage, .1, "average wrong");
+            Assert.AreEqual(expectedStdDev, actualStdDev, .1, "deviation wrong");
+        }
+    }
+}
