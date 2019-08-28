480ba6b68a7d69e77f621bbc5cd5e4f98f4f939c
Alfredo Inostroza <jadenguy@gmail.com>
2019-08-28 13:45:25 -0400

    Test 71 written

---
+++ tests/Common.Test/Test071.cs
@@ -0,0 +1,35 @@
+﻿// Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+
+using System.Linq;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test071
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown(){}
+        [Test]
+        public void Problem071()
+        {
+            //-- Arrange
+            var expected = 100;
+            int expectedStdDev = 10;
+            var actual = new double[5];
+
+            //-- Act
+            for (int i = 0; i < 100 * 5; i++)
+            {
+                actual[Solution071.rand5()]++;
+            }
+            double actualAvg = actual.Average();
+            double actualStdDev = Common.Extensions.MathExtensions.PopulationStandardDeviation(actual);
+
+
+
+            //-- Assert
+            Assert.AreEqual(expected, actualAvg, 10);
+            Assert.AreEqual(expectedStdDev, actualStdDev, 10);
+        }
+    }
+}
