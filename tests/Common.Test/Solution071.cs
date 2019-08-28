b05f8b3910762f99a061dd46b2d825cd84eb5a3b
Alfredo Inostroza <jadenguy@gmail.com>
2019-08-28 15:28:21 -0400

    71 done

---
+++ tests/Common.Test/Solution071.cs
@@ -0,0 +1,22 @@
+// Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+
+using System;
+
+namespace Common.Test
+{
+    public class Solution071
+    {
+        static Random rand = new Random();
+        public static int rand7() => 1 + rand.Next(7);
+        public static int rand5()
+        {
+            var ret = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                ret+=rand7();
+            }
+            ret = ret % 5;
+            return ret;
+        }
+    }
+}
--- tests/Common.Test/Test071.cs
+++ tests/Common.Test/Test071.cs
@@ -1,34 +1,35 @@
 ﻿// Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
 
 using System.Linq;
+using Common.Extensions;
 using NUnit.Framework;
 
 namespace Common.Test
 {
     public class Test071
     {
+        private const int rounds = 50;
+
         // [SetUp] public void Setup() { }
         // [TearDown] public void TearDown(){}
         [Test]
         public void Problem071()
         {
             //-- Arrange
-            var expected = 100;
-            int expectedStdDev = 10;
+            double expected = rounds;
+            double expectedStdDev = rounds / 10;
             var actual = new double[5];
 
             //-- Act
-            for (int i = 0; i < 100 * 5; i++)
+            for (int i = 0; i < rounds * 5; i++)
             {
                 actual[Solution071.rand5()]++;
             }
             double actualAvg = actual.Average();
-            double actualStdDev = Common.Extensions.MathExtensions.PopulationStandardDeviation(actual);
-
-
+            double actualStdDev = actual.PopulationStandardDeviation();
 
             //-- Assert
-            Assert.AreEqual(expected, actualAvg, 10);
+            Assert.AreEqual(expected, actualAvg);
             Assert.AreEqual(expectedStdDev, actualStdDev, 10);
         }
     }
