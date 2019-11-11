4d5156afa863b745bb2d5735351017caa017b22b
Alfredo Inostroza <jadenguy@gmail.com>
2019-11-11 16:57:33 -0500

    Test 80 written

---
+++ tests/Common.Test/Solution080.cs
@@ -0,0 +1,13 @@
+using System;
+using Common.Node;
+
+namespace Common
+{
+    public class Solution080
+    {
+        public static object Evaluate(BinaryNode<string> node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Common.Test/Test080.cs
@@ -0,0 +1,97 @@
+// Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+//     a
+//    / \
+//   b   c
+//  /
+// d
+
+
+using System.Collections.Generic;
+using Common.Node;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test080
+    {
+        List<BinaryNode<string>> nodes;
+        List<BinaryNode<string>> evaluations;
+
+        private static BinaryNode<string> n(string text, string name = null)
+        {
+            if (name is null) { name = text; }
+            return new BinaryNode<string>(value: text, name: name);
+        }
+        private BinaryNode<string> n(object v) => n(v.ToString());
+        [SetUp]
+        public void Setup()
+        {
+            nodes = new List<BinaryNode<string>>();
+            evaluations = new List<BinaryNode<string>>();
+            BinaryNode<string> root;
+
+            // 0 zero
+            nodes.Add(root = n('a'));
+            root.Left = n('b');
+            root.Right = n('c');
+            root.Left.Left = n('b');
+            evaluations.Add(root.Left.Left);
+
+
+            // // 1 add
+            // nodes.Add(root = n(a));
+            // root.Left = n(1);
+            // root.Right = n(2);
+            // evaluations.Add(3);
+
+            // // 2 subtract
+            // nodes.Add(root = n(s));
+            // root.Left = n(3);
+            // root.Right = n(1);
+            // evaluations.Add(2);
+
+            // // 3 multiply
+            // nodes.Add(root = n(m));
+            // root.Left = n(2);
+            // root.Right = n(3);
+            // evaluations.Add(6);
+
+            // // 4 divide
+            // nodes.Add(root = n(d));
+            // root.Left = n(6);
+            // root.Right = n(2);
+            // evaluations.Add(3);
+
+            // // 5 (3 + 2) * (4 + 5)
+            // var x = (3 + 2) * (4 + 5);
+            // nodes.Add(root = n(m));
+            // root.Left = n(a);
+            // root.Right = n(a);
+            // root.Left.Left = n(3);
+            // root.Left.Right = n(2);
+            // root.Right.Left = n(4);
+            // root.Right.Right = n(5);
+            // evaluations.Add(x);
+        }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCase(0)]
+        // [TestCase(1)]
+        // [TestCase(2)]
+        // [TestCase(3)]
+        // [TestCase(4)]
+        // [TestCase(5)]
+        public void Problem080(int testCase)
+        {
+            //-- Arrange
+            var expected = evaluations[testCase];
+            var node = nodes[testCase];
+
+            //-- Act
+            var actual = Solution080.Evaluate(node);
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
