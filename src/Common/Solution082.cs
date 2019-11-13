d9bc00ce9c1abbe1ad19eb9cb56b9ae0b480d704
Alfredo Inostroza <jadenguy@gmail.com>
2019-11-13 09:03:02 -0500

    82 finished

--- src/Common/Solution082.cs
+++ src/Common/Solution082.cs
@@ -15,24 +15,29 @@ namespace Common
             }
             public string Read7()
             {
-                var x = header * 7 + 7;
-                var ret = this.text.Substring(header * 7, 7);
+                const int SEVEN = 7;
+                int startIndex = header * SEVEN;
+                System.Diagnostics.Debug.WriteLine(header);
                 header++;
-                return ret;
+                var remaining = Math.Min(text.Length - startIndex, SEVEN);
+                if (remaining > 0) { return this.text.Substring(startIndex, remaining); }
+                else { return string.Empty; }
             }
             public void Reset()
             {
                 header = 0;
             }
+            public override string ToString() => $"{text} {text.Length}";
         }
         public static string ReadN(File file, int n)
         {
             var ret = string.Empty;
-            for (int i = 0; i <= n / 7; i++)
+            for (int i = 0; i < (n / 7d); i++)
             {
                 ret += file.Read7();
             }
-            return ret.Substring(0, n);
+            if (!string.IsNullOrEmpty(ret)) { return ret.Substring(0, Math.Min(n, ret.Length)); }
+            else { return string.Empty; }
         }
     }
 }
--- tests/Common.Test/Test082.cs
+++ tests/Common.Test/Test082.cs
@@ -1,8 +1,6 @@
 // Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
 // For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.
 
-using System.Collections.Generic;
-using System.Linq;
 using NUnit.Framework;
 
 namespace Common.Test
@@ -12,14 +10,17 @@ namespace Common.Test
         // [SetUp] public void Setup() { }
         // [TearDown] public void TearDown() { }
         [Test]
-        public void Problem082()
+        [TestCase("1234567", 6, "123456")]
+        [TestCase("1234567", 7, "1234567")]
+        [TestCase("1234567", 8, "1234567")]
+        [TestCase("Hello World", 13, "Hello World")]
+        public void Problem082(string content = "Hello World", int n = 11, string results = "Hello World")
         {
             //-- Arrange
-            var file = new Solution082.File("Hello World");
-            var expected = "Hello World";
+            var file = new Solution082.File(content);
+            var expected = results;
 
             //-- Act
-            const int n = 11;
             var actual = Solution082.ReadN(file, n);
 
             // //-- Assert
