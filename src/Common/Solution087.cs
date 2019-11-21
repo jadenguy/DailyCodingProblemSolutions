8c9a1577a9b21d96d4e32010482ae0ef42b47b08
Alfredo Inostroza <jadenguy@gmail.com>
2019-11-21 14:11:51 -0500

    Wrote up test 87

---
+++ src/Common/Solution087.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Common
+{
+    public class Solution087
+    {
+        public static int Validate(string[] ruleSets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Common.Test/Test087.cs
@@ -0,0 +1,54 @@
+﻿// A rule looks like this:
+// A NE B
+// This means this means point A is located northeast of point B.
+// A SW C
+// means that point A is southwest of C.
+// Given a list of rules, check if the sum of the rules validate. For example:
+// A N B
+// B NE C
+// C N A
+// does not validate, since A cannot be both north and south of C.
+// A NW B
+// A N B
+// is considered valid.
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test087
+    {
+        List<string[]> ruleSets;
+        List<bool> results;
+        [SetUp]
+        public void Setup()
+        {
+            ruleSets = new List<string[]>();
+            results = new List<bool>();
+            ruleSets.Add(new string[] { "A NE B" });
+            results.Add(true);
+            ruleSets.Add(new string[] { "A SW C" });
+            results.Add(true);
+            ruleSets.Add(new string[] { "A N B", "B NE C", "C N A" });
+            results.Add(false);
+            ruleSets.Add(new string[] { "A NW B", "A N B" });
+            results.Add(true);
+        }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCase(0)]
+        public void Problem087(int index)
+        {
+            //-- Arrange
+            string[] ruleSet = ruleSets[index];
+            var expected = results[index];
+
+            //-- Act
+            int actual = Solution087.Validate(ruleSet);
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
