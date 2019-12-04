09d422bc39704085066009f74e583da06ffdfd73
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-04 15:45:00 -0500

    Bellman ford was a bust

--- src/Common/Solution094.cs
+++ src/Common/Solution094.cs
@@ -9,18 +9,32 @@ namespace Common
     {
         public static int LongestPathSum(BinaryNode<int> node, bool checkNegative = true)
         {
-            var ret = 0;
-            if (node is null)
-            { throw new System.Exception("null node checked"); }
-            else
+            double ret = 0;
+            if (node is null) { throw new System.Exception("null node checked"); }
+            var graph = CreateGraph(node);
+            foreach (var item in graph.Keys)
             {
-                GraphNode graph = CreateGraph(node)
+                var table = graph.Values.ToDictionary(k => k, v => double.PositiveInfinity);
+                table[graph[item]] = -item.Data;
+                double max = -table.BellmanFord().Values.Min();
+                ret = Math.Max(ret, max);
+            }
+            return (int)ret;
+        }
+        private static Dictionary<BinaryNode<int>, GraphNode> CreateGraph(BinaryNode<int> node)
+        {
+            var ret = node.BreadthFirstSearch().ToDictionary(k => k, v => new GraphNode(v.Name));
+            foreach (var parent in ret.Keys)
+            {
+                var p = ret[parent];
+                foreach (var child in parent.Children())
+                {
+                    var c = ret[child];
+                    p.ConnectTo(c, -child.Data);
+                    // c.ConnectTo(p, -parent.Data);
+                }
             }
             return ret;
-        }
-        private static GraphNode CreateGraph(BinaryNode<int> node)
-        {
-            throw new NotImplementedException();
         }
     }
 }
