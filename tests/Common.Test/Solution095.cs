8529bf94d95ec70f008b7f4008784eb6567c4c01
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-05 12:34:35 -0500

    test 95 written

---
+++ tests/Common.Test/Solution095.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Common
+{
+    public class Solution095
+    {
+        internal static int[] NextLexicographically(int[] numerator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Common.Test/Test095.cs
@@ -0,0 +1,30 @@
+// Given a number represented by a list of digits, find the next greater permutation of a number, in terms of lexicographic ordering.If there is not greater permutation possible, return the permutation with the lowest value/ordering.
+// For example, the list [1,2,3] should return [1,3,2]. The list [1,3,2] should return [2,1,3]. The list [3,2,1] should return [1,2,3].
+// Can you perform the operation without allocating extra memory (disregarding the input memory)?
+
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test095
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 })]
+        [TestCase(new int[] { 1, 3, 2 }, new int[] { 2, 1, 3 })]
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        public void Problem095(int[] input, int[] output)
+        {
+            //-- Arrange
+            var expected =  output;
+
+            //-- Act
+            var actual = Solution095.NextLexicographically(input);
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
