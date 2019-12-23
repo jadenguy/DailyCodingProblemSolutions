088b43b60668b9fcad11f7046dea9cf24bb9f4fe
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-23 15:08:02 -0500

    Added two tests

--- src/Common/Solution105.cs
+++ src/Common/Solution105.cs
@@ -7,15 +7,18 @@ namespace Common
 {
     public static class Solution105
     {
-        private static Dictionary<Func<int, int>, DateTime> dictionary;
+        public static Dictionary<Func<int, int>, Task<int>> list = new Dictionary<Func<int, int>, Task<int>>();
         public static Task<int> Debounce(Func<int, int> func, int number, int time)
         {
-            return Task.Run(() =>
+            if (!list.ContainsKey(func))
+            {
+                list[func] = Task.Run(() =>
                 {
                     Thread.Sleep(time);
                     return func(number);
-                }
-            );
+                });
+            }
+            return list[func];
         }
     }
 }
--- tests/Common.Test/Test105.cs
+++ tests/Common.Test/Test105.cs
@@ -11,17 +11,34 @@ namespace Common.Test
         // [SetUp] public void Setup() { }
         // [TearDown] public void TearDown() { }
         [Test]
-        public void Problem105()
+        public void Problem105Debounce()
         {
             //-- Arrange
             var expected = 0;
+            int milliseconds = 100;
+            int bounces = 10;
             Func<int, int> func = e => e;
 
             //-- Act
-            int milliseconds = 500;
-            int bounces = 10;
-            var awaiter = Solution105.Debounce(func, 0, milliseconds); ;
+            var awaiter = Solution105.Debounce(func, 0, milliseconds);
             for (int i = 1; i < bounces; i++) { awaiter = Solution105.Debounce(func, i, milliseconds); }
+            var actual = awaiter.GetAwaiter().GetResult();
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Problem105AfterDebounce()
+        {
+            //-- Arrange
+            var expected = 1;
+            Func<int, int> func = e => e;
+            int milliseconds = 100;
+
+            //-- Act
+            var awaiter = Solution105.Debounce(func, 0, milliseconds);
+            System.Threading.Tasks.Task.Delay(101);
+            awaiter = Solution105.Debounce(func, 1, milliseconds);
             var actual = awaiter.GetAwaiter().GetResult();
 
             // //-- Assert
