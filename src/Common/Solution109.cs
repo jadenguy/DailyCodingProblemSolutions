f83e2e8033ac41868151081776f27be67c427eb3
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-30 11:16:24 -0500

    Added comments to 109

--- src/Common/Solution109.cs
+++ src/Common/Solution109.cs
@@ -6,7 +6,11 @@ namespace Common
     {
         public static byte SwapOddEven(byte input)
         {
-            return (byte)(((0b10101010 & input) >> 1) | ((0b01010101 & input) << 1));
+            // order of operation puts the bitshift first
+            // then the AND operation, so this is a push
+            // and grab, rather than a grab and push,
+            // otherwise you lop off a bit at the end
+            return (byte)((0b10101010 & input << 1) | (0b01010101 & input >> 1));
         }
     }
 }
