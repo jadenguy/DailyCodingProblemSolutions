b586ff3932fbf613b9887befefa5c032fb77b8a3
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-30 12:51:26 -0500

    110 solved

--- src/Common/Solution110.cs
+++ src/Common/Solution110.cs
@@ -1,14 +1,16 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Common.Node;
 
 namespace Common
 {
     public static class Solution110
     {
-        public static IEnumerable<IEnumerable<int>> EnumerateBranches(BinaryNode<string> node)
+        public static IEnumerable<IEnumerable<int>> EnumerateBranches(this BinaryNode<int> node, IEnumerable<int> parent = null)
         {
-            throw new NotImplementedException();
+            var ret = new List<int>(parent ?? new int[0]) { node.Value };
+            if (node.Children().Any()) { return node.Children().SelectMany(n => n.EnumerateBranches(ret)); }
+            else { return new int[][] { ret.ToArray() }; }
         }
     }
 }
--- tests/Common.Test/Test110.cs
+++ tests/Common.Test/Test110.cs
@@ -8,6 +8,9 @@
 // Return [[1, 2], [1, 3, 4], [1, 3, 5]].
 
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Extensions;
 using Common.Node;
 using NUnit.Framework;
 
@@ -20,24 +23,35 @@ namespace Common.Test
         [Test]
         [TestCaseSource(typeof(Cases))]
         // [TestCaseSource(typeof(CasesTwo))]
-        public void Problem110(BinaryNode<string> node, string result)
+        public void Problem110(BinaryNode<int> node, int[][] result)
         {
             //-- Arrange
             var expected = result;
-            System.Console.WriteLine("");
-            System.Diagnostics.Debug.WriteLine("");
-            System.Console.WriteLine(node?.Print(n => n.Value.ToString()));
-            System.Diagnostics.Debug.WriteLine(node?.Print(n => n.Value.ToString()));
-            System.Console.WriteLine(expected);
-            System.Diagnostics.Debug.WriteLine(expected);
+            PrintInputs(node, expected);
 
             //-- Act
-            var actual = Solution110.EnumerateBranches(node);
-            System.Console.WriteLine(actual);
-            System.Diagnostics.Debug.WriteLine(actual);
+            var actual = Solution110.EnumerateBranches(node).ToArray();
+            PrintOutput(actual);
 
             // //-- Assert
             Assert.AreEqual(expected, actual);
+        }
+        private static void PrintOutput(IEnumerable<IEnumerable<int>> actual)
+        {
+            string paths = actual.Select(p => p.Print(",")).Print();
+            System.Console.WriteLine("");
+            System.Diagnostics.Debug.WriteLine("");
+            System.Console.WriteLine(paths);
+            System.Diagnostics.Debug.WriteLine(paths);
+        }
+        private static void PrintInputs(BinaryNode<int> node, int[][] expected)
+        {
+            System.Console.WriteLine("");
+            System.Diagnostics.Debug.WriteLine("");
+            var tree = node?.Print(n => n.Value.ToString());
+            System.Console.WriteLine(tree);
+            System.Diagnostics.Debug.WriteLine(tree);
+            PrintOutput(expected);
         }
         private static BinaryNode<int> n(int data) => new BinaryNode<int>(data);
         class Cases : IEnumerable
@@ -45,6 +59,7 @@ namespace Common.Test
             public IEnumerator GetEnumerator()
             {
                 BinaryNode<int> root;
+                BinaryNode<int> current;
                 int[][] result;
 
                 // 0
@@ -54,6 +69,18 @@ namespace Common.Test
                 root.Right.Left = n(4);
                 root.Right.Right = n(5);
                 result = new int[][] { new int[] { 1, 2 }, new int[] { 1, 3, 4 }, new int[] { 1, 3, 5 } };
+                yield return new object[] { root, result };
+
+                // 1
+                root = n(0);
+                current = root;
+                const int nodeCount = 6;
+                for (int i = 1; i <= nodeCount; i++)
+                {
+                    current.Right = n(i);
+                    current = current.Right;
+                }
+                result = new int[][] { Enumerable.Range(0, nodeCount + 1).ToArray() };
                 yield return new object[] { root, result };
             }
         }
@@ -65,7 +92,6 @@ namespace Common.Test
         //     {
         //         BinaryNode<string> root = null;
         //         String result = null;
-
         //         // by using a constant seed here, we can better use automatic testing
         //         // the reason is that the name of the test changes based on the text of the object
         //         var rand = new Random(Seed);
