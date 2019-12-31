5e1b0f843bd1785e84aea910adb3342ac8f249f5
Alfredo Inostroza <jadenguy@gmail.com>
2019-12-31 12:09:20 -0500

    Test 112 written

---
+++ src/Common/Solution112.cs
@@ -0,0 +1,13 @@
+using System;
+using Common.Node;
+
+namespace Common
+{
+    public static class Solution112
+    {
+        public static BinaryNode<T> FindLowestCommonAncestor<T>(BinaryNode<T> root, BinaryNode<T> a, BinaryNode<T> b )
+        {
+            return root;
+        }
+    }
+}
---
+++ tests/Common.Test/Test112.cs
@@ -0,0 +1,117 @@
+﻿// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree. Assume that each node in the tree also has a pointer to its parent.
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+
+using System.Collections;
+using Common.Node;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test112
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCaseSource(typeof(Cases))]
+        // [TestCaseSource(typeof(CasesTwo))]
+        public void Problem112(BinaryNode<int> root, BinaryNode<int> a, BinaryNode<int> b, BinaryNode<int> lca)
+        {
+            //-- Arrange
+            var expected = lca;
+            PrintInputs(root, a, b, expected);
+
+            //-- Act
+            var actual = Solution112.FindLowestCommonAncestor(root, a, b);
+            WriteOutput(actual);
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        private static void PrintInputs(BinaryNode<int> node, BinaryNode<int> a, BinaryNode<int> b, BinaryNode<int> expected)
+        {
+            var tree = node?.Print(n => n.Value.ToString());
+            WriteHost(tree);
+            WriteHost(a);
+            WriteHost(b);
+            WriteHost(expected);
+        }
+
+        private static void WriteHost(object tree, string header = "")
+        {
+            WriteOutput(header);
+            WriteOutput(tree);
+        }
+
+        private static void WriteOutput(object Value)
+        {
+            System.Console.WriteLine(Value.ToString());
+            System.Diagnostics.Debug.WriteLine(Value.ToString());
+        }
+
+        private static BinaryNode<int> n(int data) => new BinaryNode<int>(data);
+        class Cases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                BinaryNode<int> root;
+                BinaryNode<int> a;
+                BinaryNode<int> b;
+                BinaryNode<int> result;
+
+
+                root = n(1);
+                root.Left = n(2);
+                root.Right = n(3);
+                root.Left.Left = n(4);
+                root.Right.Left = n(5);
+                root.Right.Right = n(6);
+
+                a = root.Left;
+                b = root.Right;
+                result = root;
+                yield return new object[] { root, a, b, result };
+
+                a = root;
+                b = root.Right;
+                result = root;
+                yield return new object[] { root, a, b, result };
+
+                a = root.Right;
+                b = root.Right;
+                result = root.Right;
+                yield return new object[] { root, a, b, result };
+
+                a = root.Left.Left;
+                b = root.Right.Left;
+                result = root;
+                yield return new object[] { root, a, b, result };
+
+                a = root.Right.Left;
+                b = root.Right.Right;
+                result = root.Right;
+                yield return new object[] { root, a, b, result };
+            }
+        }
+        // class CasesTwo : IEnumerable
+        // {
+        //     private const int TestCount = 10;
+        //     private const int Seed = 0;
+        //     public IEnumerator GetEnumerator()
+        //     {
+        //         BinaryNode<string> root = null;
+        //         String result = null;
+        //         // by using a constant seed here, we can better use automatic testing
+        //         // the reason is that the name of the test changes based on the text of the object
+        //         var rand = new Random(Seed);
+        //         for (int i = 0; i < TestCount; i++)
+        //         {
+        //             var enumerable = Enumerable.Range(0, TestCount).Select(r => rand.Next().ToString("X8")).Distinct();
+        //             root = BinarySearchNode<string>.GenerateBinarySearchNode(enumerable);
+        //             result = root.BreadthFirstSearch().Select(n => n.Value).Print(", ");
+        //             yield return new object[] { root, result };
+        //         }
+        //     }
+        // }
+    }
+}
