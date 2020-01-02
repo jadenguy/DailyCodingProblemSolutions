dbdf3d17965d40f9d99d0f0465792717b02f5d4f
Alfredo Inostroza <jadenguy@gmail.com>
2020-01-02 12:48:06 -0500

    Test 113 written

---
+++ src/Common/Solution113.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    public static class Solution113
+    {
+        public static string ReverseWordOrder(string input, char delimiter = ' ')
+        {
+        }
+    }
+}
---
+++ tests/Common.Test/Test113.cs
@@ -0,0 +1,30 @@
+// Given a string of words delimited by spaces, reverse the words in string. For example, given "hello world here", return "here world hello"
+// Follow-up: given a mutable string representation, can you perform this operation in-place?
+
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test113
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCase("hello world hello", "hello world hello")]
+        public void Problem113(string input, string result)
+        {
+            //-- Arrange
+            input.WriteHost("input");
+            result.WriteHost("result");
+            var expected = result;
+
+            //-- Act
+            var actual = Solution113.ReverseWordOrder(input);
+            actual.WriteHost("results");
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
