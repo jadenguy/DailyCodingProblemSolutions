b5e0cb02c3927df4765ef487416645471a46c006
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-07 17:03:32 -0500

    114 solved?

--- src/Common/Solution114.cs
+++ src/Common/Solution114.cs
@@ -1,12 +1,45 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Extensions;
 
 namespace Common
 {
     public class Solution114
     {
-        public static string ReverseWordOrder(string input, string delimiters)
+        public static string ReverseWordOrderDelimiterList(string input, string delimiters)
         {
-            throw new NotImplementedException();
+            int length = input.Length;
+            var current = input.FirstOrDefault();
+            var wasChar = delimiters.Contains(current);
+            var startsChar = wasChar;
+            var words = new Queue<string>();
+            var wordStart = 0;
+            for (int i = 1; i < length; i++)
+            {
+                current = input[i];
+                var isChar = delimiters.Contains(current);
+                var wasSeries = isChar ^ wasChar;
+                if (wasSeries)
+                {
+                    var word = input.Substring(wordStart, i - wordStart);
+                    words.Enqueue(word);
+                    wordStart = i;
+                }
+                wasChar = isChar;
+            }
+            words.Enqueue(input.Substring(wordStart, length - wordStart));
+            var ret = new List<string>();
+            if (!startsChar) { ret.Add(words.Dequeue()); }
+            var wordStack = new Stack<string>(words);
+            int count = words.Count;
+            for (int i = 0; i < count; i++)
+            {
+                bool isEven = i % 2 == 0;
+                var top = words.Dequeue();
+                var bottom = wordStack.Pop();
+                ret.Add(isEven ? top : bottom);
+            }
+            return ret.Print("");
         }
     }
 }
