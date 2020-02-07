cd440d15c51852148ae8b598e322e64a8e583bb8
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-07 10:02:35 -0500

    test 114 written

---
+++ src/Common/Solution114.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Common
+{
+    public class Solution114
+    {
+        public static string ReverseWordOrder(string input, string delimiters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Common.Test/Test114.cs
@@ -0,0 +1,31 @@
+// Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters. For example, given "hello/world:here", return "here/world:hello"
+// Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
+
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test114
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCase("hello/world:here/", "/:", "hello//world:here")]
+        [TestCase("hello/world:here", "/:", "here/world:hello")]
+        public void Problem114(string input, string delimiters, string result)
+        {
+            //-- Arrange
+            input.WriteHost("input");
+            result.WriteHost("desired");
+            var expected = result;
+
+            //-- Act
+            var actual = Solution114.ReverseWordOrder(input, delimiters);
+            actual.WriteHost("results");
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
