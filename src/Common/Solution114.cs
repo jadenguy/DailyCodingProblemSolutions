60d1b722e1fb9becbe50fca575369c019dcc998a
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-10 12:55:48 -0500

    114 finished

--- src/Common/Solution114.cs
+++ src/Common/Solution114.cs
@@ -6,40 +6,67 @@ namespace Common
 {
     public class Solution114
     {
-        public static string ReverseWordOrderDelimiterList(string input, string delimiters)
+        public static string ReverseWordOrderDelimiterList(string input, string delimiterSet)
         {
-            int length = input.Length;
+            var phrases = SeparatePhrases(input, delimiterSet, out var startDelimited);
+            SeparateWordsReverseAndDelimiters(phrases, startDelimited, out var wordsReverse, out var delimiters);
+            var ret = ZipWordsReverseAndDelimiters(startDelimited, wordsReverse, delimiters);
+            return ret.Print("");
+        }
+        private static List<string> ZipWordsReverseAndDelimiters(bool startsDelimited, Stack<string> wordsReverse, Queue<string> delimiters)
+        {
+            var ret = new List<string>();
+            if (startsDelimited) { ret.Add(delimiters.Dequeue()); }
+            while (wordsReverse.Count > 0)
+            {
+                string word = wordsReverse.Pop();
+                ret.Add(word);
+                try
+                {
+                    string del = delimiters.Dequeue();
+                    ret.Add(del);
+                }
+                catch (System.Exception) { }
+            }
+
+            return ret;
+        }
+        private static void SeparateWordsReverseAndDelimiters(IEnumerable<string> phrases, bool startsDelimited, out Stack<string> wordsReverse, out Queue<string> delimiters)
+        {
+            wordsReverse = new Stack<string>();
+            delimiters = new Queue<string>();
+            var isDelimiter = startsDelimited;
+            foreach (var word in phrases)
+            {
+                if (isDelimiter) { delimiters.Enqueue(word); }
+                else { wordsReverse.Push(word); }
+                isDelimiter = !isDelimiter;
+            }
+        }
+        private static IEnumerable<string> SeparatePhrases(string input, string delimiters, out bool startDelimited)
+        {
+            var length = input.Length;
+            var ret = new Queue<string>();
             var current = input.FirstOrDefault();
-            var wasChar = delimiters.Contains(current);
-            var startsChar = wasChar;
+            var wasDelimiter = delimiters.Contains(current);
+            startDelimited = wasDelimiter;
             var words = new Queue<string>();
             var wordStart = 0;
             for (int i = 1; i < length; i++)
             {
                 current = input[i];
-                var isChar = delimiters.Contains(current);
-                var wasSeries = isChar ^ wasChar;
+                var isDelimiter = delimiters.Contains(current);
+                var wasSeries = isDelimiter ^ wasDelimiter;
                 if (wasSeries)
                 {
                     var word = input.Substring(wordStart, i - wordStart);
                     words.Enqueue(word);
                     wordStart = i;
                 }
-                wasChar = isChar;
+                wasDelimiter = isDelimiter;
             }
             words.Enqueue(input.Substring(wordStart, length - wordStart));
-            var ret = new List<string>();
-            if (!startsChar) { ret.Add(words.Dequeue()); }
-            var wordStack = new Stack<string>(words);
-            int count = words.Count;
-            for (int i = 0; i < count; i++)
-            {
-                bool isEven = i % 2 == 0;
-                var top = words.Dequeue();
-                var bottom = wordStack.Pop();
-                ret.Add(isEven ? top : bottom);
-            }
-            return ret.Print("");
+            return words;
         }
     }
 }
