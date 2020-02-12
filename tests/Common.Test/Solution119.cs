739e25a94f58bdd25ee4fd55ca82b63917620781
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-12 14:46:38 -0500

    119 solved, need better test

---
+++ tests/Common.Test/Solution119.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using Common.Extensions;
+
+namespace Common
+{
+    public class Solution119
+    {
+        public static IEnumerable<int> NumbersCoveringIntervals((int start, int end)[] input)
+        {
+            var inputSet = new HashSet<(int, int)>(input);
+            var start = input.Min(n => n.end);
+            var end = input.Max(n => n.start);
+            var possibleCoveringIntegers = Enumerable.Range(start, end - start + 1);
+
+            var coveredIntervals = possibleCoveringIntegers.ToDictionary(k => k, v => input.Where(r => r.start <= v && r.end >= v));
+
+            var possibleSets = coveredIntervals.Keys.EverySubset();
+            var possibleAnswers = possibleSets.ToDictionary(k => k, n => new HashSet<(int, int)>(n.SelectMany(g => coveredIntervals[g])));
+            var answers = possibleAnswers.Where(v => v.Value.SetEquals(inputSet)).OrderBy(k => k.Key.Length);
+
+            return answers.FirstOrDefault().Key.OrderBy(n => n);
+        }
+    }
+}
--- tests/Common.Test/Test119.cs
+++ tests/Common.Test/Test119.cs
@@ -1,9 +1,8 @@
 ﻿// Given a set of closed intervals, find the smallest set of numbers that covers all the intervals. If there are multiple smallest sets, return any of them.
 // For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers that covers all these intervals is {3, 6}.
 
-
-
 using System.Collections;
+using System.Linq;
 using Common.Extensions;
 using NUnit.Framework;
 
@@ -18,7 +17,7 @@ namespace Common.Test
         public void Problem119((int, int)[] input, int[] result)
         {
             //-- Arrange
-            var expected = result;
+            var expected = result.OrderBy(n => n);
             input.Print(",").WriteHost("Source");
             expected.Print(",").WriteHost("Expected");
 
@@ -29,32 +28,17 @@ namespace Common.Test
             // //-- Assert
             Assert.AreEqual(expected, actual);
         }
-
         class Cases : IEnumerable
         {
             private const int testCountPer = 10;
-
+            private System.Random rand = new System.Random(119);
             public IEnumerator GetEnumerator()
             {
-                //seeded to assist testing fixtures
-                // var rand = new System.Random(119);
-                // var root = ArbitraryTreeBinaryNode.GenerateArbitaryBinaryTreeNode(rand.Next());
-
-                // for (int i = 0; i < testCountPer; i++)
-                // {
-                //     var other = ArbitraryTreeBinaryNode.GenerateArbitaryBinaryTreeNode(rand.Next(), "Other");
-                //     yield return new object[] { root, other, false };
-
-                //     var subRand = new System.Random(rand.Next());
-                //     var sub = root.BreadthFirstSearch().Random(subRand).ToArray().FirstOrDefault();
-                //     yield return new object[] { root, sub, true };
-
                 (int, int)[] x;
                 int[] y;
                 x = new (int, int)[] { (0, 3), (2, 6), (3, 4), (6, 9) };
                 y = new int[] { 3, 6 };
                 yield return new object[] { x, y };
-
             }
         }
     }
