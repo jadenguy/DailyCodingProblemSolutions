cda030f265ced0e1896016439e0d54c56aef8004
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-13 11:50:36 -0500

    Test 121 written

---
+++ src/Common/Solution121.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Common
+{
+    public static class Solution121
+    {
+        public static bool PalindromeDeletionPossible(string input, int deletionMax)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Common.Test/Test121.cs
@@ -0,0 +1,32 @@
+﻿// Given a string which we can delete at most k, return whether you can make a palindrome.
+// For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
+
+
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test121
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCase("waterrfetawx", 2, true)]
+        public void Problem121(string input, int deletionMax, bool isPossible)
+        {
+            //-- Arrange
+            var expected = isPossible;
+            input.WriteHost("Source");
+            deletionMax.WriteHost("Deletions");
+            expected.WriteHost("Expected");
+
+            //-- Act
+            bool actual = Solution121.PalindromeDeletionPossible( input, deletionMax);
+            actual.WriteHost("Actual");
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
