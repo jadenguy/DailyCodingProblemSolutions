cb981168e03b002a8410f53d057bdffbcc992084
Alfredo Inostroza <jadenguy@gmail.com>
2020-02-25 13:10:52 -0500

    129 test written

--- tests/Common.Test/Test126.cs
+++ tests/Common.Test/Test126.cs
@@ -1,10 +1,6 @@
 ﻿// You have n fair coins and you flip them all at the same time. Any that come up tails you set aside. The ones that come up heads you flip again. How many rounds do you expect to play before only one coin remains?
 // Write a function that, given n, returns the number of rounds you'd expect to play until one coin remains.
 
-
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Common.Extensions;
 using NUnit.Framework;
 
@@ -20,7 +16,7 @@ namespace Common.Test
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, -4, new int[] { 3, 4, 5, 6, 1, 2 })]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 2, new int[] { 3, 4, 5, 6, 1, 2 })]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2, new int[] { 3, 4, 5, 6, 7, 1, 2 })]
-        public void Problem126Individually(int[] a, int k, int[] b)
+        public void Problem126(int[] a, int k, int[] b)
         {
             //-- Arrange
             var expected = b;
---
+++ tests/Common.Test/Test129.cs
@@ -0,0 +1,50 @@
+﻿// Given a real number n, find the square root of n. For example, given n = 9, return 3
+
+using System;
+using System.Collections;
+using System.Numerics;
+using Common.Extensions;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test129
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCaseSource(typeof(Cases))]
+        public void Problem129(double n, Complex sqRt)
+        {
+            //-- Arrange
+            var expected = sqRt;
+            n.WriteHost("Square");
+            expected.WriteHost("Square Root");
+
+            //-- Act
+            var actual = Solution129.SqRt(n);
+            actual.WriteHost("Calculated Square Root");
+
+            // //-- Assert
+            Assert.AreEqual(expected.Real, actual.Real, .0000001 * sqRt.Real, "Real Component Accurate");
+            Assert.AreEqual(expected.Imaginary, actual.Imaginary, .0000001 * sqRt.Imaginary, "Imaginary Component Wrong");
+        }
+        class Cases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { -12345 * 12345, 12345 * Complex.ImaginaryOne };
+                yield return new object[] { -Math.PI * Math.PI, Math.PI * Complex.ImaginaryOne };
+                yield return new object[] { -4, 2 * Complex.ImaginaryOne };
+                yield return new object[] { -1, 1 * Complex.ImaginaryOne };
+                yield return new object[] { 0, 0 * Complex.ImaginaryOne };
+                yield return new object[] { .25, .5 * Complex.One };
+                yield return new object[] { 1, 1 * Complex.One };
+                yield return new object[] { 9, 3 * Complex.One };
+                yield return new object[] { Math.PI * Math.PI, Math.PI * Complex.One };
+                yield return new object[] { 12345 * 12345, 12345 * Complex.One };
+                yield break;
+            }
+        }
+    }
+}
