eeb70087ff9ec7d89b748e79556489618ee52722
Alfredo Inostroza <jadenguy@gmail.com>
2020-03-06 16:15:32 -0500

    Shell of 132 written

---
+++ src/Common/Solution132.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Common
+{
+    public class Solution132
+    {
+        public class HitCounter
+        {
+            public HitCounter()
+            {
+            }
+
+            public void Hit(DateTime now)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ tests/Common.Test/Test132.cs
@@ -0,0 +1,40 @@
+﻿// Design and implement a HitCounter class that keeps track of requests (or hits). It should support the following operations:
+// •	record(timestamp): records a hit that happened at timestamp
+// •	total(): returns the total number of hits recorded
+// •	range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
+// Follow-up: What if our system has limited memory?
+
+using System.Collections;
+using NUnit.Framework;
+
+namespace Common.Test
+{
+    public class Test132
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        [TestCaseSource(typeof(Cases))]
+        public void Problem132()
+        {
+            //-- Arrange
+            var expected = new object();
+            var counter = new Solution132.HitCounter();
+            
+            //-- Act
+            counter.Hit(System.DateTime.Now);
+            var actual = counter;
+            
+
+            // //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        class Cases : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { };
+            }
+        }
+    }
+}
