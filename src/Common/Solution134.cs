371f17a0c18846a13ec333ab8313125581199191
Alfredo Inostroza <jadenguy@gmail.com>
2020-03-11 09:55:45 -0400

    134 done with added functionality

--- src/Common/Solution134.cs
+++ src/Common/Solution134.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,29 +6,43 @@ namespace Common
 {
     public static class Solution134
     {
-        public class SparseArray<T>
+        public class SparseArray<T> : IEnumerable<T>
         {
+            public const string SparseArrayExceptionMessage = "SparseArrayException";
+            private Dictionary<int, T> realValues { get; } = new Dictionary<int, T>();
+            public int Length { get; }
+            public int StorageSize { get => realValues.Count(); }
             public SparseArray(T[] array)
             {
-                Size = array.Length;
-                for (int i = 0; i < Size; i++)
+                Length = array.Length;
+                for (int i = 0; i < Length; i++)
                 {
                     Set(i, array[i]);
                 }
             }
-            private Dictionary<int, T> realValues { get; } = new Dictionary<int, T>();
-            public int Size { get; }
-            public int StorageSize { get => realValues.Count(); }
+            public SparseArray(T[] array, int length) : this(array)
+            {
+                Length = length;
+                realValues.Keys.Where(k => k >= length).Select(k => realValues.Remove(k));
+            }
+            public SparseArray(int length) => this.Length = length;
+            public IEnumerable<(int Key, T Value)> GetValues() => realValues.OrderBy(n => n.Key).Select(x => (x.Key, x.Value));
+            public T this[int i] { get => Get(i); set => Set(i, value); }
             public void Set(int index, T value)
             {
+                ValidateIndex(index);
                 if (value.Equals(default(T))) { realValues.Remove(index); }
                 else { realValues[index] = value; }
             }
-            public T Get(int v)
+            public T Get(int index)
             {
-                realValues.TryGetValue(v, out var r);
+                ValidateIndex(index);
+                realValues.TryGetValue(index, out var r);
                 return r;
             }
+            private void ValidateIndex(int index) { if (index >= Length || index < 0) { throw new System.IndexOutOfRangeException(SparseArrayExceptionMessage); } }
+            public IEnumerator<T> GetEnumerator() => Enumerable.Range(0, Length).Select(i => Get(i)).GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
         }
     }
 }
--- tests/Common.Test/Test134.cs
+++ tests/Common.Test/Test134.cs
@@ -4,7 +4,7 @@
 // •	set(i, val): updates index at i with val.
 // •	get(i): gets the value at index i.
 
-
+using System;
 using System.Linq;
 using NUnit.Framework;
 using Common.Extensions;
@@ -14,6 +14,8 @@ namespace Common.Test
     public class Test134
     {
         private const int Count = 5;
+        private const int V1 = 1;
+        private const int V2 = Count + 2;
 
         // [SetUp] public void Setup() { }
         // [TearDown] public void TearDown() { }
@@ -22,25 +24,39 @@ namespace Common.Test
         {
             //-- Arrange
             //an array that begins with a pair of 0, has the digits 2 through 6 sprinkled around a then ten thousand empty spots
-            int[] first = (new[] { 0, 0 });
-            var second = Enumerable.Range(2, Count).Concat(Enumerable.Repeat(0, 10000)).Shuffle(134).ToArray();
-            var array = first.Concat(second).ToArray();
-            var size = array.Count();
-            var a = new Solution134.SparseArray<int>(array);
-            var expectedValue = 1;
+            int[] zeroValues = (new[] { 0, 0, 0 });
+            var someValues = Enumerable.Range(2, Count).Concat(Enumerable.Repeat(0, 10000)).Shuffle(134).ToArray();
+            var array = zeroValues.Concat(someValues).ToArray();
+            var spareArray = new Solution134.SparseArray<int>(array);
+            var expectedValue = V1;
+            var expectedValueTwo = V2;
             var expectedNoValue = 0;
-            var expectedCount = Count + 1;
+            var expectedCount = Count + 2;
 
             //-- Act
-            a.Set(0, 1);
-            var actualValue = a.Get(0);
-            var actualNoValue = a.Get(1);
-            var actualCount = 6;
+            spareArray.Set(0, V1);
+            array[0] = V1;
+            var actualValue = spareArray.Get(0);
+            spareArray[1] = V2;
+            array[1] = V2;
+            var actualValueTwo = spareArray[1];
+            var actualNoValue = spareArray[2];
+            var actualCount = spareArray.StorageSize;
+            var values = spareArray.GetValues().ToArray();
+            (bool largeIndex, bool negativeIndex) = (false, false);
+            try { spareArray[array.Length] = 1; }
+            catch (IndexOutOfRangeException oor) { largeIndex = (oor.Message) == Solution134.SparseArray<int>.SparseArrayExceptionMessage; }
+            try { spareArray[-1] = 1; }
+            catch (IndexOutOfRangeException oor) { negativeIndex = (oor.Message) == Solution134.SparseArray<int>.SparseArrayExceptionMessage; }
 
             //-- Assert
             Assert.AreEqual(expectedValue, actualValue);
+            Assert.AreEqual(expectedValueTwo, actualValueTwo);
             Assert.AreEqual(expectedNoValue, actualNoValue);
+            Assert.IsTrue(negativeIndex);
+            Assert.IsTrue(largeIndex);
             Assert.AreEqual(expectedCount, actualCount);
+            Assert.IsTrue(array.SequenceEqual(spareArray));
         }
     }
 }
