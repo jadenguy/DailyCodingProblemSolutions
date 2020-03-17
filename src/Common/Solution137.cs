e8094d8f88266c75e1939ed970dbfc26ae8b2c77
Alfredo Inostroza <jadenguy@gmail.com>
2020-03-17 11:06:39 -0400

    test 137 written

---
+++ src/Common/Solution137.cs
@@ -0,0 +1,49 @@
+﻿// Implement a bit array.
+// A bit array is a space efficient array that holds a value of 1 or 0 at each index.
+// •	init(size): initialize the array with size
+// •	set(i, val): updates index at i with val where val is either 1 or 0.
+// •	get(i): gets the value at index i.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    public class Solution137
+    {
+        public class BitArray
+        {
+            public int Size { get; }
+            public int StorageSize { get => internalByteArray.Length; }
+            private int[] internalByteArray;
+            public bool this[int index] { get => Get(index); set => Set(index, value); }
+            public BitArray(int size)
+            {
+                Size = size;
+                internalByteArray = new int[size / 32];
+            }
+            public void Set(int index, bool value)
+            {
+                (int byteAddress, int bitAddress) = GetAddress(index);
+                var bits = internalByteArray[byteAddress];
+                var bit = bitAddress;
+            }
+            public bool Get(int index)
+            {
+                (int byteAddress, int bitAddress) = GetAddress(index);
+                throw new NotImplementedException();
+            }
+            public IEnumerable<bool> GetValues() => Enumerable.Range(0, Size).Select(i => Get(i));
+            private (int byteAddress, int bitAddress) GetAddress(int index)
+            {
+                if (index >= Size) { throw new IndexOutOfRangeException(); }
+                int byteAddress = index / 32;
+                int bitAddress = index % 32;
+                return (byteAddress, bitAddress);
+            }
+            
+        }
+    }
+}
---
+++ tests/Common.Test/Test137.cs
@@ -0,0 +1,56 @@
+﻿// Implement a bit array.
+// A bit array is a space efficient array that holds a value of 1 or 0 at each index.
+// •	init(size): initialize the array with size
+// •	set(i, val): updates index at i with val where val is either 1 or 0.
+// •	get(i): gets the value at index i.
+
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Common.Extensions;
+
+namespace Common.Test
+{
+    public class Test137
+    {
+        const int size = 128;
+        const bool V1 = true;
+        const bool V2 = false;
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        [Test]
+        public void Problem137()
+        {
+            //-- Arrange
+            var bitArray = new Solution137.BitArray(size);
+            var array = bitArray;
+            var expectedValue = V1;
+
+            //-- Act
+            bitArray.Set(0, V1);
+            array[0] = V1;
+            var actualValue = bitArray.Get(0);
+            bitArray[1] = V2;
+            array[1] = V2;
+            var actualValueTwo = bitArray[1];
+            var actualNoValue = bitArray[2];
+            var actualCount = bitArray.StorageSize;
+            var values = bitArray.GetValues().ToArray();
+            (bool largeIndex, bool negativeIndex) = (false, false);
+            // try { bitArray[array.Length] = 1; }
+            // catch (IndexOutOfRangeException oor) { largeIndex = (oor.Message) == Solution137.SparseArray<int>.SparseArrayExceptionMessage; }
+            // try { bitArray[-1] = 1; }
+            // catch (IndexOutOfRangeException oor) { negativeIndex = (oor.Message) == Solution137.SparseArray<int>.SparseArrayExceptionMessage; }
+
+            //-- Assert
+            Assert.AreEqual(expectedValue, actualValue);
+            // Assert.AreEqual(expectedValueTwo, actualValueTwo);
+            // Assert.AreEqual(expectedNoValue, actualNoValue);
+            Assert.IsTrue(negativeIndex);
+            Assert.IsTrue(largeIndex);
+            // Assert.AreEqual(expectedCount, actualCount);
+            // Assert.IsTrue(array.SequenceEqual(bitArray));
+        }
+    }
+}
