f122ac072ccbd88999e89b89c40972efc4ecf0b7
Alfredo Inostroza <jadenguy@gmail.com>
2020-04-22 15:49:52 -0400

    two more imported tests

---
+++ tests/import/Test384.cs
@@ -0,0 +1,25 @@
+// You are given an array of integers representing coin denominations and a total amount of money. Write a function to compute the fewest number of coins needed to make up that amount. If it is not possible to make that amount, return null.
+// For example, given an array of [1, 5, 10] and an amount 56, return 7 since we can use 5 dimes, 1 nickel, and 1 penny.
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Common.Extensions;
+
+namespace Common.Test
+{
+    public class Test384
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        //[Test]
+        public void Problem384()
+        {
+            //-- Assert
+
+            //-- Arrange
+
+            //-- Act
+            Assert.Pass();
+        }
+    }
+}
---
+++ tests/import/Test385.cs
@@ -0,0 +1,27 @@
+// You are given a hexadecimal-encoded string that has been XOR'd against a single char.
+// Decrypt the message. For example, given the string:
+// 7a575e5e5d12455d405e561254405d5f1276535b5e4b12715d565b5c551262405d505e575f
+// You should be able to decrypt it and get:
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Common.Extensions;
+
+namespace Common.Test
+{
+    public class Test385
+    {
+        // [SetUp] public void Setup() { }
+        // [TearDown] public void TearDown() { }
+        //[Test]
+        public void Problem385()
+        {
+            //-- Assert
+
+            //-- Arrange
+
+            //-- Act
+            Assert.Pass();
+        }
+    }
+}
