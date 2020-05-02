b5a19eb3d82b2f73e88ae753c20a5fabb4984612
Alfredo Inostroza <jadenguy@gmail.com>
2020-05-02 19:21:30 -0400

    Rand takes a null seed now

--- src/Common/001-020/Random.cs
+++ src/Common/001-020/Random.cs
@@ -1,10 +1,11 @@
 namespace Common
 {
     public static class Rand
+    {
+        public static System.Random NewRandom(this int? seed) => NewRandom(0);
+        public static System.Random NewRandom(this int seed)
         {
-            public static System.Random NewRandom(this int seed)
-            {
-                return seed == 0 ? new System.Random() : new System.Random(seed);
-            }
+            return seed == 0 ? new System.Random() : new System.Random(seed);
         }
+    }
 }
