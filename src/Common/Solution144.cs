d81968bcb9e740dbbde200ffc494efb016f34e08
Alfredo Inostroza <jadenguy@gmail.com>
2020-05-06 15:32:01 -0400

    144 solved

--- src/Common/Solution144.cs
+++ src/Common/Solution144.cs
@@ -4,9 +4,22 @@ namespace Common
 {
     public static class Solution144
     {
-        public static int FindNearestLargeNumberIndexDelta(int[] array, int index)
+        public static int? FindNearestLargeNumberIndexDelta(int[] array, int index)
         {
-            throw new NotImplementedException();
+            var yardstick = array[index];
+            var delta = 1;
+            while (true)
+            {
+                int v = index + delta;
+                int v1 = index - delta;
+                int length = array.Length;
+                bool v2 = length > v;
+                bool v3 = 0 <= v1;
+                if (v2 && array[v] > yardstick) { return v; }
+                else if (v3 && array[v1] > yardstick) { return v1; }
+                else if (!(v2 | v3)) { return null; }
+                delta++;
+            }
         }
     }
 }
--- tests/Common.Test/Test144.cs
+++ tests/Common.Test/Test144.cs
@@ -2,6 +2,8 @@
 // For example, given [4, 1, 3, 5, 6] and index 0, you should return 3.
 // If two distances to larger numbers are the equal, then return any one of them. If the array at i doesn't have a nearest larger integer, then return null.
 
+using System.Collections;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Common.Test
@@ -10,19 +12,28 @@ namespace Common.Test
     {
         // [SetUp] public void Setup() { }
         // [TearDown] public void TearDown() { }
-        //[Test]
-        public void Problem144()
+        [Test]
+        [TestCaseSource(typeof(Cases144))]
+        public static void Problem144(int[] array, int index, int[] result)
         {
             //-- Assert
-            var expected = 3;
-            var array = new[] { 4, 1, 3, 5, 6 };
-            var index = 0;
+            var expected = result;
 
             //-- Arrange
-            int actual = Solution144.FindNearestLargeNumberIndexDelta(array, index);
+            var actual = Solution144.FindNearestLargeNumberIndexDelta(array, index);
 
             //-- Act
-            Assert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.Any(v => v == actual));
+        }
+        private class Cases144 : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                var expected = new[] { 3 };
+                var array = new[] { 4, 1, 3, 5, 6 };
+                var index = 0;
+                yield return new object[] { array, index, expected };
+            }
         }
     }
 }
